e05744382c467799b835efecc3b59d1f4b683526
Aleksandra Teska <aleksandra.teska@gmail.com>
2017-12-01 18:13:13 +0100

    coffee exapmle extended, names changes, added slope to the plot

--- CN11.py
+++ CN11.py
@@ -1,4 +1,4 @@
-#class 1
+#class 1 -  differential equations - coffee example
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -6,14 +6,14 @@ C = 1e3
 Tr = 20
 K = 0.2 # * W/K
 
-def dTcoffee(K, C, Tr, Tc):
-    return K/C * (Tr - Tc)
-
 def dTcSteadState(K, C, Tr, dTcoffee):
     return Tr - dTcoffee * C/K
 
-def Tcoffee(t, K, C, Tr, Tc0):
+def analytic_dTcoffee(t, K, C, Tr, Tc0):
     return Tr + (Tc0 - Tr) * np.exp(-K/C * t)
+
+def dTcoffee(K, C, Tr, Tc):
+    return K/C * (Tr - Tc)
 
 dt = 1
 l = int(30000/dt)
@@ -27,11 +27,16 @@ for n in np.arange(0,l-1,1):
     T[n+1] = T[n] + dT * dt
     t[n+1] = n+1
 
-print (t)
-print (T)
+slope = (T[1] - T[0]) / dt
+line = lambda t: slope*t + T[0]
+tslope = t[:5000]
 
-fig = plt.plot(t, T)
-#plt.ylim([0,2])
-plt.grid(True)
-plt.ioff()
+#plot
+fig, ax = plt.subplots()
+ax.plot(t, T, label="temp")
+ax.plot(tslope, line(tslope), label="tau")
+ax.set_xlabel('t')
+ax.set_ylabel('coffee temp')
+ax.set_title('Nummerical solution for coffee state');
+ax.grid(True)
 plt.show()
